a2fc2d25c5b1da58f6ec02178852b9be4f5db0c6
DashhaSep <sarkisovadara365@gmail.com>
2025-06-26 14:21:45 +0300

    final laptop

--- javascripts/exhibition.js
+++ javascripts/exhibition.js
@@ -4,9 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
   openModalTwo();
   openModal();
   AfterPurchase();
+  AfterPurchase2();
+  AfterPurchase3();
+  AfterPurchase4();
   getTicket();
   // updateDisplay();
   showBurgerMenu();
+  getTicket2();
+  getTicket3();
+  getTicket4();
 });
 // окно регистрации
 function openModal() {
@@ -112,6 +118,37 @@ function AfterPurchase() {
     modal.style.display = "none";
   });
 }
+function AfterPurchase2() {
+  document.getElementById("submitBtn2").addEventListener("click", function () {
+    alert(
+      "Благодарим за покупку! Дальнейшая форма оплаты направлена вам на почту!"
+    );
+    const modal = document.querySelector(".modal2");
+
+    modal.style.display = "none";
+  });
+}
+function AfterPurchase3() {
+  document.getElementById("submitBtn3").addEventListener("click", function () {
+    alert(
+      "Благодарим за покупку! Дальнейшая форма оплаты направлена вам на почту!"
+    );
+    const modal = document.querySelector(".modal3");
+
+    modal.style.display = "none";
+  });
+}
+function AfterPurchase4() {
+  document.getElementById("submitBtn4").addEventListener("click", function () {
+    alert(
+      "Благодарим за покупку! Дальнейшая форма оплаты направлена вам на почту!"
+    );
+    const modal = document.querySelector(".modal4");
+
+    modal.style.display = "none";
+  });
+}
+// для первого
 function getTicket() {
   let count = 1;
   const pricePerTicket = 500;
@@ -125,6 +162,81 @@ function getTicket() {
   });
 
   document.getElementById("minus").addEventListener("click", () => {
+    if (count > 0) {
+      count--;
+      updateDisplay();
+    }
+  });
+
+  function updateDisplay() {
+    countDisplay.textContent = count;
+    totalDisplay.textContent = count * pricePerTicket;
+  }
+}
+// для второго
+function getTicket2() {
+  let count = 1;
+  const pricePerTicket = 500;
+
+  const countDisplay = document.getElementById("count2");
+  const totalDisplay = document.getElementById("total2");
+
+  document.getElementById("plus2").addEventListener("click", () => {
+    count++;
+    updateDisplay();
+  });
+
+  document.getElementById("minus2").addEventListener("click", () => {
+    if (count > 0) {
+      count--;
+      updateDisplay();
+    }
+  });
+
+  function updateDisplay() {
+    countDisplay.textContent = count;
+    totalDisplay.textContent = count * pricePerTicket;
+  }
+}
+// для третьего
+function getTicket3() {
+  let count = 1;
+  const pricePerTicket = 500;
+
+  const countDisplay = document.getElementById("count3");
+  const totalDisplay = document.getElementById("total3");
+
+  document.getElementById("plus3").addEventListener("click", () => {
+    count++;
+    updateDisplay();
+  });
+
+  document.getElementById("minus3").addEventListener("click", () => {
+    if (count > 0) {
+      count--;
+      updateDisplay();
+    }
+  });
+
+  function updateDisplay() {
+    countDisplay.textContent = count;
+    totalDisplay.textContent = count * pricePerTicket;
+  }
+}
+// для четвертого
+function getTicket4() {
+  let count = 1;
+  const pricePerTicket = 500;
+
+  const countDisplay = document.getElementById("count4");
+  const totalDisplay = document.getElementById("total4");
+
+  document.getElementById("plus4").addEventListener("click", () => {
+    count++;
+    updateDisplay();
+  });
+
+  document.getElementById("minus4").addEventListener("click", () => {
     if (count > 0) {
       count--;
       updateDisplay();
